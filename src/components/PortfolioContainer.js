fed7464f9b5ab26a4cacedc49fa7979b959825e4
Amber Zimmerman <ajz1221@gmail.com>
2022-05-17 16:37:30 -0600

    fadded logic to render which page

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -2,5 +2,34 @@ import React, { useState } from "react";
 import Nav from "./Nav";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Blog from "./pages/Blog";
 import Contact from "./pages/Contact";
+import Resume from "./pages/Resume";
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState("Home");
+
+  const renderPage = () => {
+    if (currentPage === "Home") {
+      return <Home />;
+    }
+    if (currentPage === "About") {
+      return <About />;
+    }
+    if (currentPage === "Contact") {
+      return <Contact />;
+    }
+    if (currentPage === "Resume") {
+      return <Resume />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
+}
