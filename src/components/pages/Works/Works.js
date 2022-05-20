9316176a2a08102111278c6ce8b36605ccd5cebd
Amber Zimmerman <ajz1221@gmail.com>
2022-05-20 12:22:24 -0600

    creating project object

--- src/components/pages/Works/Works.js
+++ src/components/pages/Works/Works.js
@@ -17,13 +17,23 @@ export default function Works() {
   };
 
   return (
-    <section style={{ height: "800px", display: "flex" }}>
-      <div style={leftWorks}>
-        <h1>Hiiiiiiiiii</h1>
-      </div>
-      <div style={rightWorks}>
-        <div style={worksGrid}></div>
-      </div>
+    // <section style={{ height: "800px", display: "flex" }}>
+    //   <div style={leftWorks}>
+    //     <h1>Hiiiiiiiiii</h1>
+    //   </div>
+    //   <div style={rightWorks}>
+    //     <div style={worksGrid}>
+    //       <div
+    //     </div>
+    //   </div>
+    // </section>
+
+    <section className="container">
+      <aside>
+        <h2>THIS IS ALL MY PROJECTS!</h2>
+        <p>blah blah blah here are my projects</p>
+      </aside>
+      <div className="card"></div>
     </section>
   );
 }
---
+++ src/projects.js
@@ -0,0 +1,48 @@
+const projects = [
+  {
+    project1: {
+      title: "Stacking Your Squad",
+      description: "Command-line application that helps a user to build a team",
+      technology: [
+        "Javascript",
+        "HTML/CSS",
+        "Command-line Application",
+        "Node.js",
+        "Inquirer",
+        "Jest",
+      ],
+      image: "yes",
+      demogif: "yes",
+      github: "https://github.com/AmberZimmerman/stacking_your_squad",
+      deployed: "",
+      videolink:
+        "https://drive.google.com/file/d/1dkVYjO1oYt4Z6aMn3xa9273rPobtBAv-/view?usp=sharing",
+    },
+  },
+  {
+    project2: {
+      title: "Secure Password Generator",
+      description:
+        "User can generate their own secure password by choosing specific criteria based on given options. ",
+      technology: ["Javascript", "HTML/CSS"],
+      image: "yes",
+      demogif: "",
+      github: "https://github.com/AmberZimmerman/secure-password-generator",
+      deployed: "",
+      videolink: "",
+    },
+  },
+  {
+    project3: {
+      title: "Dolly Work Day Planner",
+      description:
+        "A Dolly Parton themed day planner that allows a user to schedule in hourly tasks. Hourly tasks will change background color in real time. Tasks can be saved to local storage.",
+      technology: ["Javascript", "HTML/CSS", "Local Storage"],
+      image: "",
+      demogif: "",
+      github: "https://github.com/AmberZimmerman/dolly-workday-planner",
+      deployed: "https://github.com/AmberZimmerman/dolly-workday-planner",
+      videolink: "",
+    },
+  },
+];
