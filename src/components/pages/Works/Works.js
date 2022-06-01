6e9c5864d034581063ce36e768ea7fb73a42550c
Amber Zimmerman <ajz1221@gmail.com>
2022-06-01 12:40:35 -0600

    getting overlay and buttons to appear for works cards

--- src/components/pages/Works/Works.js
+++ src/components/pages/Works/Works.js
@@ -5,6 +5,8 @@ export default function Works({ projects }) {
   const worksStyle = {
     backgroundColor: "black",
     color: "white",
+    display: "flex",
+    justifyContent: "center",
   };
 
   return (
@@ -12,32 +14,36 @@ export default function Works({ projects }) {
       <div className="works-grid">
         {projects.map((project) => (
           <div className="card" key={project.title}>
-            <img
-              className="works-image"
-              src={project.image}
-              alt="project screenshot"
-            />
-
-            <div className="button-container">
-              <button
-                type="button"
-                onClick={(e) => {
-                  e.preventDefault();
-                  window.location.href = `${project.github}`;
-                }}
-              >
-                Github
-              </button>
-              <button
-                type="button"
-                onClick={(e) => {
-                  e.preventDefault();
-                  window.location.href = `${project.demo}`;
-                }}
-              >
-                {`${project.demotype}`}
-              </button>
+            <div className="image-container">
+              <div className="image-overlay">
+                <div className="button-container">
+                  <button
+                    type="button"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      window.location.href = `${project.github}`;
+                    }}
+                  >
+                    Github
+                  </button>
+                  <button
+                    type="button"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      window.location.href = `${project.demo}`;
+                    }}
+                  >
+                    {`${project.demotype}`}
+                  </button>
+                </div>
+              </div>
+              <img
+                className="works-image"
+                src={project.image}
+                alt="project screenshot"
+              />
             </div>
+
             <h4>{`${project.title}`}</h4>
 
             <p>{`${project.technology}`}</p>
