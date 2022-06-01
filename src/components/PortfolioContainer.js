0fbc70aad19614eb88e3aff9996483e6757a501a
Amber Zimmerman <ajz1221@gmail.com>
2022-06-01 14:02:52 -0600

    fixing some styling on about section, changing name of home to about and the bio component to bio

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Nav from "./Nav";
-import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
 import Works from "./pages/Works/Works";
 import Contact from "./pages/Contact/Contact";
 import Resume from "./pages/Resume";
@@ -8,15 +8,12 @@ import projects from "../projects";
 import Footer from "./Footer";
 
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState("HOME");
+  const [currentPage, setCurrentPage] = useState("ABOUT");
 
   const renderPage = () => {
-    if (currentPage === "HOME") {
-      return <Home />;
+    if (currentPage === "ABOUT") {
+      return <About />;
     }
-    // if (currentPage === "ABOUT") {
-    //   return <About />;
-    // }
     if (currentPage === "WORKS") {
       return <Works projects={projects} />;
     }
--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Lefthero from "../../Left-hero";
 import Heroimage from "../../Hero-image";
-import About from "../../About";
-import "./Home.css";
+import Bio from "../../Bio";
+import "./About.css";
 
 export default function Home({ currentPage, handlePageChange }) {
   return (
     <section className="hero home-grid">
       <Lefthero />
       <Heroimage />
-      <About currentPage={currentPage} handlePageChange={handlePageChange} />
+      <Bio currentPage={currentPage} handlePageChange={handlePageChange} />
     </section>
   );
 }
