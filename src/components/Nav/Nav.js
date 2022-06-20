4beb5b63ff62ac551ef043a2365a5061c4942556
Amber Zimmerman <ajz1221@gmail.com>
2022-06-19 19:46:49 -0600

    getting all mobile view changes finished for about page

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,50 +3,14 @@ import "./Nav.css";
 
 export default function Nav({ currentPage, handlePageChange }) {
   return (
-    <div style={{ flexWrap: "wrap", height: "50px" }}>
+    <div className="nav-space">
       <nav className="nav-style">
-        <section
-          style={{
-            textAlign: "center",
-            fontSize: "20px",
-            justifyContent: "space-evenly",
-            width: "100%",
-            display: "flex",
-            height: "50px",
-          }}
-        >
-          <div
-            className="portfolio"
-            style={{
-              backgroundColor: "black",
-              height: "100%",
-              display: "flex",
-              width: "32%",
-              justifyContent: "center",
-            }}
-          >
-            <h1
-              style={{
-                fontSize: "30px",
-                color: "white",
-                textAlign: "center",
-                margin: "0px",
-                fontFamily: "Raleway, sans-serif",
-                alignSelf: "center",
-              }}
-            >
-              PORTFOLIO
-            </h1>
+        <section className="nav-section-style">
+          <div className="portfolio left-nav">
+            <h1 className="portfolio-word">PORTFOLIO</h1>
           </div>
 
-          <div
-            style={{
-              display: "flex",
-              width: "68%",
-              backgroundColor: "rgb(5, 51, 53)",
-              justifyContent: "center",
-            }}
-          >
+          <div className="right-nav">
             <ul style={{ display: "flex" }}>
               <li className="link-style">
                 <a
@@ -56,20 +20,9 @@ export default function Nav({ currentPage, handlePageChange }) {
                     currentPage === "ABOUT" ? "nav-link active" : "nav-link"
                   }
                 >
-                  HOME
-                </a>
-              </li>
-              {/* <li className="link-style">
-                <a
-                  href="#about"
-                  onClick={() => handlePageChange("ABOUT")}
-                  className={
-                    currentPage === "ABOUT" ? "nav-link active" : "nav-link"
-                  }
-                >
                   ABOUT
                 </a>
-              </li> */}
+              </li>
               <li className="link-style">
                 <a
                   href="#works"
